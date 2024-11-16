4753a763947b6f7c0b197518d3a99a3f9a5134a1
robert <robertrivastorres@gmail.com>
2024-11-16 17:57:58 -0500

    Front Completo

--- src/app/componentes/contenedor/Inicio.tsx
+++ src/app/componentes/contenedor/Inicio.tsx
@@ -1,10 +1,22 @@
-export const Inicio = ()=>{
-    return(
-        <>
-        <br />
-        <br />
-        Soy el Inicio <br />
-        Como el de Bienvenida...
-        </>
-    )
-}
+import { NavLink } from "react-router-dom";
+export const Inicio = () => {
+  return (
+    <>
+      <main>
+      <div className="container px-4 py-5" id="featured-3">
+    <h2 className="pb-2 border-bottom"><i className="bi bi-music-note-list"></i> Bienvenidos <i className="bi bi-music-note-list"></i> </h2>
+    <div className="row g-4 py-5 row-cols-1 row-cols-lg-3">
+      <div className="feature col">
+        <div className="feature-icon d-inline-flex align-items-center justify-content-center  bg-gradient fs-2 mb-3">
+        <i className="bi bi-person-raised-hand"></i>
+        </div>
+        <h3 className="fs-2 text-body-emphasis">Hola</h3>
+        <p>Estas en la pagina inicio</p>
+        
+      </div>
+    </div>
+  </div>
+      </main>
+    </>
+  );
+};
--- src/app/componentes/otros/Acerca.tsx
+++ src/app/componentes/otros/Acerca.tsx
@@ -1,9 +1,113 @@
 export const Acerca = () => {
     return (
       <>
-        Acerca de <br />
-        El de la información...
-      </>
+      <main>
+        <section className="py-5 text-center container">
+          <div className="row py-lg-5">
+            <div className="col-lg-6 col-md-8 mx-auto">
+              <h1 className="fw-light">CONOCENOS!!</h1>
+              <p className="lead text-body-secondary">
+                Aqui puedes saber acerca de nosotros
+              </p>
+            </div>
+          </div>
+        </section>
+
+        <div className="album py-5 bg-body-tertiary">
+          <div className="container">
+            <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+              <div className="col">
+                <div className="card shadow-sm">
+                  <svg
+                    className="bd-placeholder-img card-img-top"
+                    width="100%"
+                    height="225"
+                    xmlns="http://www.w3.org/2000/svg"
+                    role="img"
+                    aria-label="Placeholder: Artista #1"
+                    preserveAspectRatio="xMidYMid slice"
+                    focusable="false"
+                  >
+                    <title>Placeholder</title>
+                    <rect width="100%" height="100%" fill="aqua"></rect>
+                    <text x="50%" y="50%" fill="#000000" dy=".3em">
+                      Que somos
+                    </text>
+                  </svg>
+                  <div className="card-body">
+                    <p className="card-text">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint quae 
+                      ipsam nisi omnis, asperiores aut est labore excepturi itaque.
+                    </p>
+                    <div className="d-flex justify-content-between align-items-center">
+                      <small className="text-body-secondary"><i className="bi bi-music-note fa-2x"></i></small>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col">
+                <div className="card shadow-sm">
+                  <svg
+                    className="bd-placeholder-img card-img-top"
+                    width="100%"
+                    height="225"
+                    xmlns="http://www.w3.org/2000/svg"
+                    role="img"
+                    aria-label="Placeholder: Thumbnail"
+                    preserveAspectRatio="xMidYMid slice"
+                    focusable="false"
+                  >
+                    <title>Placeholder</title>
+                    <rect width="100%" height="100%" fill="aqua"></rect>
+                    <text x="50%" y="50%" fill="#00000" dy=".3em">
+                      Nuestro objetivo
+                    </text>
+                  </svg>
+                  <div className="card-body">
+                    <p className="card-text">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint quae 
+                    ipsam nisi omnis, asperiores aut est labore excepturi itaque.
+                    </p>
+                    <div className="d-flex justify-content-between align-items-center">
+                      <small className="text-body-secondary"><i className="bi bi-music-note fa-2x"></i></small>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col">
+                <div className="card shadow-sm">
+                  <svg
+                    className="bd-placeholder-img card-img-top"
+                    width="100%"
+                    height="225"
+                    xmlns="http://www.w3.org/2000/svg"
+                    role="img"
+                    aria-label="Placeholder: Thumbnail"
+                    preserveAspectRatio="xMidYMid slice"
+                    focusable="false"
+                  >
+                    <title>Placeholder</title>
+                    <rect width="100%" height="100%" fill="aqua"></rect>
+                    <text x="50%" y="50%" fill="#00000" dy=".3em">
+                      Nuestra vision
+                    </text>
+                  </svg>
+                  <div className="card-body">
+                    <p className="card-text">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint quae 
+                    ipsam nisi omnis, asperiores aut est labore excepturi itaque.
+                    </p>
+                    <div className="d-flex justify-content-between align-items-center">
+                      <small className="text-body-secondary"><i className="bi bi-music-note fa-2x"></i></small>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    </>
     );
   };
   
